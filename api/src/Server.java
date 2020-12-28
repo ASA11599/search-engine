e4d76ca1bf7324a6cf8f003cdfc4585aa3b7c4a2
Ubuntu <saidameramayas@gmail.com>
2020-12-28 15:47:52 -0500

    Update API server

--- api/src/Server.java
+++ api/src/Server.java
@@ -11,47 +11,48 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Server {
 
-    private static byte[] handleRequest(HttpExchange t) {
-        String response;
-        int statusCode;
-        switch (t.getRequestURI().toString()) {
-            case "/":
-                t.getResponseHeaders().add("Content-Type", "application/json");
-                statusCode = 200;
-                response = "{\"result\": \"This is the root URL\"}";
-                break;
-            default:
-                t.getResponseHeaders().add("Content-Type", "application/json");
-                response = "{\"error\": \"No resource on this path\"}";
-                statusCode = 404;
-                break;
-        }
-        try {
-            t.sendResponseHeaders(statusCode, response.length());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return response.getBytes();
-    }
-
     private static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            byte[] response = Server.handleRequest(t);
+            String response;
+            int statusCode;
+            switch (t.getRequestURI().getPath()) {
+                case "/":
+                    t.getResponseHeaders().add("Content-Type", "application/json");
+                    response = "{\"result\": \"This is the root URL\"}";
+                    statusCode = 200;
+                    break;
+                case "/search":
+                    t.getResponseHeaders().add("Content-Type", "application/json");
+                    String q = t.getRequestURI().getQuery();
+                    response = "{\"result\": \"Congratulations, you have hit the search API\", \"query\": \"" + q + "\"}";
+                    statusCode = 200;
+                    break;
+                default:
+                    t.getResponseHeaders().add("Content-Type", "application/json");
+                    response = "{\"error\": \"No resource on this path\", \"status\": \"404\"}";
+                    statusCode = 404;
+                    break;
+            }
+            try {
+                t.sendResponseHeaders(statusCode, response.length());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             OutputStream os = t.getResponseBody();
-            os.write(response);
+            os.write(response.getBytes());
             os.close();
         }
     }
 
     public static void serve() {
         try {
-        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), 80), 0);
-        server.createContext("/", new MyHandler());
-        server.setExecutor(null);
-        server.start();
+            HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), 80), 0);
+            server.createContext("/", new MyHandler());
+            server.setExecutor(null);
+            server.start();
         } catch (IOException ioe) {
-            System.out.println("IOException");
+            ioe.printStackTrace();
         }
     }
 
