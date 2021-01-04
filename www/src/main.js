73a296a7945ca17bc5c0e7f3cc8f311b4b366106
Ubuntu <saidameramayas@gmail.com>
2021-01-04 15:26:27 -0500

    Move front-end JS code to its own file

---
+++ www/src/main.js
@@ -0,0 +1,18 @@
+const res = document.getElementById("res");
+document.getElementById("btn").onclick = (_ev) => {
+    fetch("/api/search?q=" + encodeURIComponent(document.getElementById("query").value))
+    .then(response => response.json())
+    .then((json) => {
+        document.getElementById("res").innerHTML = "";
+        for (item of json) {
+            let newElement = document.createElement("a");
+            newElement.setAttribute("href", item["link"]);
+            newElement.innerText = item["title"];
+            document.getElementById("res").appendChild(newElement);
+            document.getElementById("res").appendChild(document.createElement("br"));
+        }
+    })
+    .catch((reason) => {
+        res.innerText = reason;
+    });
+};
